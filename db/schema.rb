03a15d5deff6e5f04969cdab160d1e3315bba529
anselm <anselm@hook.org>
2009-05-06 18:33:35 -0700

    here is the same image support for teams as well

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 7) do
+ActiveRecord::Schema.define(:version => 8) do
 
   create_table "images", :force => true do |t|
     t.string   "name"
@@ -67,6 +67,9 @@ ActiveRecord::Schema.define(:version => 7) do
     t.datetime "roomtime"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
   end
 
   create_table "users", :force => true do |t|
