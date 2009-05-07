709d201728719aa34740bef97563570f00722790
anselm <anselm@hook.org>
2009-05-06 18:24:04 -0700

    settled on paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+
 class User < ActiveRecord::Base
   acts_as_authentic
 end
@@ -6,4 +7,14 @@ class User
   # roles are hardcoded TODO we could be more flexible
   ROLE_PARTICIPANT = "participant"
   ROLE_CAPTAIN = "captain"
+
+  # Paperclip
+  has_attached_file :photo,
+    :styles => {
+      :thumb=> "100x100#",
+      :small  => "150x150>",
+      :medium => "300x300>",
+      :large => "400x400>" 
+    }
+
 end
