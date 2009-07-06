615a8088289bcfa01c44ef837c2e22534ae5b55b
scottley <scott@simon.org>
2009-07-05 20:27:31 -0700

    Cleaned up user register page.  Changed database fields -- re-migrate from scratch!!

--- db/migrate/20090630170450_add_tshirt_to_users.rb
+++ db/migrate/20090630170450_add_tshirt_to_users.rb
@@ -1,7 +1,6 @@
 class AddTshirtToUsers < ActiveRecord::Migration
   def self.up
      add_column :users, :tshirt, :string
-     add_column :users, :optout, :boolean
      add_column :users, :tos, :boolean
   end
 
--- db/migrate/20090703170450_add_sex_to_users.rb
+++ db/migrate/20090703170450_add_sex_to_users.rb
@@ -4,7 +4,9 @@ class AddSexToUsers < ActiveRecord::Migration
      add_column :users, :age, :string
      add_column :users, :sex, :string
      add_column :users, :phone, :string
-     add_column :users, :sms, :boolean
+     add_column :users, :optinsms, :boolean
+     add_column :users, :optinethos, :boolean
+     add_column :users, :optinvoicebox, :boolean
   end
   def self.down
   end
