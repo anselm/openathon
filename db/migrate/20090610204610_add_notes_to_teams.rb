600f49c0911aa36d2658a96790234e2beee1674e
User <anselm@mustard.makerlab.org>
2009-07-06 10:36:04 -0700

    there was a bug with the team migration the index page had a twitter url link bug

--- db/migrate/20090610204610_add_notes_to_teams.rb
+++ db/migrate/20090610204610_add_notes_to_teams.rb
@@ -1,16 +1,9 @@
 class AddNotesToTeams < ActiveRecord::Migration
   def self.up
     change_table :notes do |t|
-      t.rename :uuid, :team_id
-      t.change :team_id, :integer
-      t.remove :provenance, :permissions, :score
+      t.integer :team_id, :default => 0
     end
   end
   def self.down
-    change_table :notes do |t|
-      t.change :team_id, :string
-      t.rename :team_id, :uuid
-      t.add :provenance, :permissions, :score
-    end
   end
 end
