a667570acb6c0a4ee17cabd325bdc435101a24e2
User <anselm@mustard.makerlab.org>
2009-07-06 22:47:00 -0700

    environment has to load yaml

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 # IMAGE_MAGICK_PATH = "/usr/local/bin/"
 
 # settings we don't want to put into git
-#SETTINGS = YAML::load(File.open("../settings.yml"))
-SETTINGS = []
+SETTINGS = YAML::load(File.open("../settings.yml"))
 SETTINGS.each do |k,v|
   sym = k.respond_to?(:to_sym) ? k.to_sym : k
   SETTINGS[sym] = v
